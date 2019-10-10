68ec36f12eba3f22d552df786547dde3528243c1
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2019-10-10 01:34:05 -0300

    Arrumando inverse.py

--- inverse.py
+++ inverse.py
@@ -23,15 +23,12 @@ fp.close()
 lines = []
 for line in lines_cmd:
     lines.append(line.rstrip())
-fa[0].machine = lines[0].split()
+fa[0].machine = lines[0]
 fa[0].input_alphabet = lines[1].split()
 fa[0].lambda_ = lines[2]
 fa[0].states = lines[3].split()
-fa[0].states = [int(p) for p in range(len(fa[0].states))]
 fa[0].initial_state = lines[4].split()
-fa[0].initial_state = [int(p) for p in range(len(fa[0].initial_state))]
-fa[0].final_states = lines[5].split()    
-fa[0].final_states = [int(p) for p in range(len(fa[0].final_states))]
+fa[0].final_states = lines[5].split()
 for j in range(6, len(lines)):
     fa[0].transitions.append(lines[j].split())
 
@@ -53,7 +50,7 @@ def printfaData(fa_aux, i):
 fa.append(FiniteAutomaton())
 
 """ Copiando o estado da máquina """
-fa[1].machine.append(fa[0].machine[0])
+fa[1].machine.append(fa[0].machine)
 
 """ Capituramos para Turing Machine União todos os alfabetos de entrada da 1a turing machine
         e na 2a turing machine são capturadas, apenas, as letras que não continha na 1a (2a linha) """
@@ -73,12 +70,13 @@ fa[1].lambda_.append(fa[0].lambda_)
 
 for i in range(len(fa[0].states)):
     fa[1].states.append(fa[0].states[i])
-fa[1].states.append(len(fa[0].states))
+fa[1].states.append("q" + str(len(fa[0].states)))
+
 #######################################################
 
 """ Definindo o estado inicial da fa União sempre como 0, que é o estado criado a mais
         que serve como controle (6a linha) """
-fa[1].initial_state.append(len(fa[0].states))
+fa[1].initial_state.append( "q" + str(len(fa[0].states)))
 
 #######################################################
 
@@ -86,41 +84,35 @@ fa[1].initial_state.append(len(fa[0].states))
         já com a referencia deles na nova fa (7a linha) """
 for i in range(len(fa[0].initial_state)):
     fa[1].final_states.append(fa[0].initial_state[i])
+
 #######################################################
 
 """ É criado asa transições inicias da TM União que define a qual máquina o conjunto de 
         entrada pertence, de acordo com a primeira letra é possível mandar para o estado
             inicias da TM 1 ou 2 ou as duas simultaneamente sem que nada seja consumido (9a linha) """
 for i in range(len(fa[0].final_states)):
-    fa[1].transitions.append(
-        (str(fa[1].states[len(fa[0].states)]) + " " + str(fa[1].lambda_[0]) + " " + str(fa[0].final_states[i])))
+    fa[1].transitions.append( 
+        (str(fa[1].initial_state[0]) + " " + str(fa[1].lambda_[0]) + " " + str(fa[0].final_states[i])).split() )
 
-#######################################################
+######################################################
 
-# """ É copiado todas as transações da fa 1 e 2, fazendo as trocas dos estados para as suas respectivas referências
-#         na fa União, e também, trocando as letras que representavam o "Branco" de cada fa 
-#             para a letra definida como "Branco" na fa União (9a linha) """
-# for j in range(2):
-#     for i in range(len(fa[j].transitions)):
+""" É copiado todas as transações da fa 1 e 2, fazendo as trocas dos estados para as suas respectivas referências
+        na fa União, e também, trocando as letras que representavam o "Branco" de cada fa 
+            para a letra definida como "Branco" na fa União (9a linha) """
+aux = []
+for i in range(len(fa[0].transitions)):
+    aux = fa[0].transitions[i][0]
+    (fa[0].transitions[i])[0] = str(fa[0].transitions[i][2])
 
-#         (fa[j].transitions[i])[0] = str(ref_states[j][int(
-#             (fa[j].transitions[i])[0], 10)])
+    (fa[0].transitions[i])[2] = str(aux)
 
-#         (fa[j].transitions[i])[1] = str(ref_states[j][int(
-#             (fa[j].transitions[i])[1], 10)])
-
-#         for k in range(2, len(fa[j].transitions[i])):
-#             if (fa[j].transitions[i])[k] == fa[j].whitespace[0]:
-#                 (fa[j].transitions[i])[k] = fa[2].whitespace[0]
-
-#         fa[2].transitions.append(fa[j].transitions[i])
+    fa[1].transitions.append(fa[0].transitions[i])
 
 
 #######################################################
 
-for i in range(2):
-    printfaData(fa[i], i)
-    print("\n\n")
+printfaData(fa[1], 1)
+print("\n\n")
 
 # """ Aberto o arquivo vindo por comando, na pasta ."/fla/turing_machine_union.txt" e escreve a união das duas fa """
 # union = open(sys.argv[3], 'w')
